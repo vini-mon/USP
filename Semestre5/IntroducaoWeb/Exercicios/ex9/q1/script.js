cb84ee37f803db9ed474ff87b85d70fe02fed63a
Vinícius Santos Monteiro <vini.mon@usp.br>
2022-05-20 14:10:25 -0300

    novas atividades de Web

---
+++ Semestre5/IntroducaoWeb/Exercicios/ex9/q1/script.js
@@ -0,0 +1,7 @@
+"use strict"
+
+let hexaColor = document.getElementById('hexaColor');
+let nameColor = document.getElementById('nameColor');
+let squareColor = document.getElementById('squareColor');
+let number = document.getElementById('number');
+let button = document.getElementById('button');
